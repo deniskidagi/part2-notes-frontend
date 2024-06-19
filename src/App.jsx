c5b9e867c15d929069615e8cd1e8eafd9075f209
deniskidagi <deniskidagi@gmail.com>
2024-06-19 20:29:31 +0300

    added styling to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react'
 import Note from './components/Note'
-import axios from 'axios';
 import noteService from './services/notes'
+import Notifications from './components/Notifications'
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('add a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
 
 
 
@@ -46,11 +48,19 @@ const App = () => {
     .then(returnedNote => {
       setNotes(notes.map(note => note.id !== id ? note : returnedNote))
     })
+    .catch(error => {
+      setErrorMessage(`Note ${note.content} was already removed from server`)
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+      setNotes(notes.filter(n => n.id !== id))
+    })
   }
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
   return (
     <div>
       <h1>Notes</h1>
+      <Notifications message={errorMessage}/>
       <div>
       <button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'All'}</button>
       </div>
@@ -63,6 +73,7 @@ const App = () => {
         <input type="text" value={newNote} onChange={handleNoteChange}/>
         <button type='submit'>save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    const footerStyle = {
+        color: 'green',
+        fontStyle: 'italics',
+        fontSize: 15
+    }
+  return (
+    <div style={footerStyle}>
+        <em>Manager DollarScripts.dev kenya 2024</em>
+    </div>
+  )
+}
+
+export default Footer
